cfbaa73dc2ca245252c46f4fe67a1f36d1df7e9c
Rohit Kisto <rohit_rtk@hotmail.com>
2026-07-10 14:25:41 -0400

    Added footer

--- src/pages/home/app.tsx
+++ src/pages/home/app.tsx
@@ -4,16 +4,22 @@ import Projects from '@/pages/home/components/projects';
 import Journey from '@/pages/home/components/journey';
 import About from '@/pages/home/components/about';
 import Contact from '@/pages/home/components/contact';
+import Footer from './components/footer';
 
 const App = () => {
   return (
     <div className="min-h-screen bg-background text-foreground">
       <Navigation />
-      <Hero />
-      <Projects />
-      <Journey />
-      <About />
-      <Contact />
+
+      <main>
+        <Hero />
+        <Projects />
+        <Journey />
+        <About />
+        <Contact />
+      </main>
+
+      <Footer />
     </div>
   );
 };
